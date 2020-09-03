7e29c5637258af959cdd9f14a12304fa43044535
Peter Nguyen <peter@saymile.com>
2020-09-03 01:30:59 -0700

    First commit

---
+++ build/precache-manifest.471b17e62637567422a984b190b57c2e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f8e4e1c10917ea7b9754a07bff6a498",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0bf4a8b8980618ffdfb",
+    "url": "/static/css/main.3c2fef4c.chunk.css"
+  },
+  {
+    "revision": "53b9518592f3e5b35e98",
+    "url": "/static/js/2.38090fac.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.38090fac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0bf4a8b8980618ffdfb",
+    "url": "/static/js/main.23e0f957.chunk.js"
+  },
+  {
+    "revision": "2a5e9718c304aba39f84",
+    "url": "/static/js/runtime-main.0f5541c3.js"
+  },
+  {
+    "revision": "b0a1eb7cef456f10aa4ae48bbf9827d8",
+    "url": "/static/media/logo.b0a1eb7c.png"
+  }
+]);
